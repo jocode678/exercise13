e00cd17f1025493a51c8d0c3cdd938eca878b932
Jo <jdaniell7272@gmail.com>
2022-03-08 15:20:49 +0000

    updates after zoom

--- read_file.py
+++ read_file.py
@@ -4,9 +4,10 @@ print(type(contents))
 print(contents)
 file.close()
 
-# Data type of output = string?
+# Data type of output = string
 
-# Do you have to repeat this first line in order to put the cursor back to the start?
+# Do you have to repeat this first line in order to put the cursor back to the start? Answer = yes
+# But IRL would use the contents variable that I created above (but this isn't what the exercise specified)
 file = open('pelican.txt', 'r')
 myfilelist = file.readlines()
 print('There are', len(myfilelist), 'items in the list.')
@@ -18,11 +19,11 @@ print('There are', len(myfilelist), 'items in the list.')
 #     print('\n', line, '\n')
 
 # Works:
-print(''.join(str(line) for line in myfilelist))
+# print(''.join(str(line) for line in myfilelist))
 
-# Doesn't work
-for line in myfilelist:
-    print(''.join(str(line)))
+# Doesn't work - applying line by line. Line has new line character, so does print function. Printing in loop.
+# for line in myfilelist:
+#     print(''.join(str(line)))
 
 # This prints number of characters after each list
 # import sys
@@ -36,5 +37,9 @@ for line in myfilelist:
 # This does not work
 # for line in myfilelist:
 #     print(line, sep='\n')
+
+# Use end not sep
+for line in myfilelist:
+    print(line, end='')
 
 file.close()
